b9dda1e8f9979251049ac8a01af7c0833228ebce
k-kamada <kamadak0404@gmail.com>
2024-04-15 15:42:46 +0900

    Add keymap to toggle terminal buffer

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -56,6 +56,9 @@ vim.opt.listchars = {
 -- Move by visual line
 vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = true, silent = true })
+-- Back from terminal
+vim.api.nvim_set_keymap('n', '<leader>t', ':bn<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<leader>t', '<C-\\><C-n>:bn<CR>', { noremap = true, silent = true })
 
 -- Indentation
 vim.opt.tabstop = 2
