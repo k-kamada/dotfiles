74ca613016dc8f60833781ec2ef2e1dac2c6ce07
k-kamada <kamadak0404@gmail.com>
2024-09-04 02:57:42 +0900

    Add Zed config

---
+++ setup_zed.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Check if XDG_CONFIG_HOME is set, if not set it to default
+if [ -z "$XDG_CONFIG_HOME" ]; then
+    XDG_CONFIG_HOME="$HOME/.config"
+fi
+
+# Create nvim directory if it doesn't exist
+mkdir -p "$XDG_CONFIG_HOME/.config/zed"
+
+# Create symlink for init.lua
+ln -sf "$HOME/dotfiles/zed/settings.json" "$XDG_CONFIG_HOME/.config/zed/settings.json"
+
+echo "Zed setup complete."
