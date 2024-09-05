222553c82fcc5ed22d64f2376f7f0177e9679daf
k-kamada <kamadak0404@gmail.com>
2024-09-05 13:14:34 +0900

    Add keymap.json

--- setup_zed.sh
+++ setup_zed.sh
@@ -10,5 +10,6 @@ mkdir -p "$XDG_CONFIG_HOME/.config/zed"
 
 # Create symlink for init.lua
 ln -sf "$HOME/dotfiles/zed/settings.json" "$XDG_CONFIG_HOME/zed/settings.json"
+ln -sf "$HOME/dotfiles/zed/keymap.json" "$XDG_CONFIG_HOME/zed/keymap.json"
 
 echo "Zed setup complete."
