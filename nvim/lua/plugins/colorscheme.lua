4816b038c8c3b85c74d3ded899b321423d098a67
k-kamada <kamadak0404@gmail.com>
2025-02-04 20:48:30 +0900

    Change nvim colorscheme to rose-pine

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-    "cocopon/iceberg.vim",
+    "rose-pine/neovim",
     lazy = false, -- 遅延ロードを無効化
     priority = 1000, -- 他のプラグインより先に読み込む
     config = function()
-        vim.cmd([[colorscheme iceberg]])
+        vim.cmd("colorscheme rose-pine")
     end,
 }
