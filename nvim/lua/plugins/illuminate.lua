23b38088bba81bcd15b0e1b1cb75aa0d552b8b75
k-kamada <kamadak0404@gmail.com>
2025-03-07 18:52:02 +0900

    Fix some settings

--- nvim/lua/plugins/illuminate.lua
+++ nvim/lua/plugins/illuminate.lua
@@ -2,6 +2,11 @@ return {
   'RRethy/vim-illuminate',
   config = function()
     require('illuminate').configure({
+      providers = {
+        'lsp',
+        'treesitter',
+        'regex',
+      },
       filetypes_denylist = {
         'dirbuf',
         'dirvish',
