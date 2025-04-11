9fe02c4840a2f6b3c446c3ebaa7ff91181c88145
k-kamada <kamadak0404@gmail.com>
2025-04-12 00:36:36 +0900

    Add toggleterm settings

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -5,6 +5,14 @@ return {
     require("toggleterm").setup({
       open_mapping = [[<c-t>]],
       hide_numbers = true,
+      shade_filetypes = {},
+      shade_terminals = true,
+      shading_factor = 2,
+      start_in_insert = true,
+      insert_mappings = true,
+      persist_size = true,
+      direction = 'float',
+      close_on_exit = true,
     })
   end
 }
