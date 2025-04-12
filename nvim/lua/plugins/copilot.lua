9b946960e4b8f098a2d440ac7f60cc4b57f4c546
k-kamada <kamadak0404@gmail.com>
2025-04-12 11:55:19 +0900

    Add shortcuts for copilot.nvim

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,4 +1,17 @@
 return {
   "github/copilot.vim",
   lazy=false,
+  config = function()
+    vim.g.copilot_no_tab_map = true,
+    vim.keymap.set('i', '<C-C>', 'copilot#Accept("\\<CR>")', {
+      expr = true,
+      replace_keycodes = false,
+      desc = "Accept copilot suggestion",
+    })
+    vim.keymap.set('i', '<C-E>', 'copilot#Dismiss()', {
+      expr = true,
+      replace_keycodes = false,
+      desc = "Dismiss copilot suggestion",
+    })
+  end,
 }
