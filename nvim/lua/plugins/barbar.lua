853a152a62d240ce48f8bff244e3307e02405bd2
k-kamada <kamadak0404@gmail.com>
2025-04-12 12:06:18 +0900

    Add shortcuts for barbar

--- nvim/lua/plugins/barbar.lua
+++ nvim/lua/plugins/barbar.lua
@@ -4,10 +4,18 @@ return {
       'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
       'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
     },
-    init = function() vim.g.barbar_auto_setup = false end,
-    map = vim.api.nvim_set_keymap,
-    opts = {
-      noremap = true,
-    },
     version = '^1.0.0', -- optional: only update when a new 1.x version is released
+    init = function() vim.g.barbar_auto_setup = false end,
+    config = function()
+      require('barbar').setup {
+      }
+      local map = vim.api.nvim_set_keymap
+      local opts = {
+        noremap = true,
+        silent = true,
+      }
+      map('n', '<C-j>', '<Cmd>BufferPrevious<CR>', opts)
+      map('n', '<C-k>', '<Cmd>BufferNext<CR>', opts)
+      map('n', '<Leader>c', '<Cmd>BufferClose<CR>', opts)
+    end,
 }
